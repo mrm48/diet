5109b2f9a4748272baf1e68cb334c98edce4ebba
Matt Miller <mmiller@crispyecho.com>
2023-11-04 06:45:34 -0400

    Implement listcalories endpoint

--- src/main/java/com/trinsic/diet3/food/FoodController.java
+++ src/main/java/com/trinsic/diet3/food/FoodController.java
@@ -1,9 +1,13 @@
 package com.trinsic.diet3.food;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 import com.trinsic.diet3.food.Food;
@@ -14,15 +18,16 @@ import com.trinsic.diet3.meal.MealService;
 @RequestMapping(path = "api/v1/food")
 public class FoodController {
     private final FoodService foodService;
-
+    private String json; 
     public FoodController(FoodService foodService){
         this.foodService = foodService;
     }
 
-	@GetMapping("/listcalories")
-	public Integer listCalories(){
-        //return foodService.listCalories();
-        return Integer.valueOf(1);
+	@RequestMapping(value = "/listcalories")
+    @ResponseBody
+	public Integer listCalories(@RequestBody Food httpEntity){
+        json = httpEntity.getName();
+        return foodService.listCalories(json);
 	}
 
     @PostMapping("/addcalories")
