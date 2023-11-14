767665eda24ec4c6a374afeecca9a09a3a2f89f2
Matt Miller <mmiller@crispyecho.com>
2023-11-13 19:25:31 -0500

    Refactor thanks to Jack

--- src/main/java/com/trinsic/diet3/meal/MealService.java
+++ src/main/java/com/trinsic/diet3/meal/MealService.java
@@ -11,40 +11,44 @@ import com.trinsic.diet3.food.Food;
 public class MealService{
 
     MealRepository mealRepository;
-    Integer calories;
 
     public MealService(MealRepository mealRepository){
         this.mealRepository = mealRepository;
     }
 
     @Transactional
-    public Integer addMeal(Meal f){
+    public Integer addMeal(Meal newMeal){
         // Only add meal if there is not a meal with the same name for the day
         Integer queryStatus = Integer.valueOf(-1);
-        Optional<Meal> searchFood = mealRepository.findMealByName(f.getName(), f.getDay());
+        Optional<Meal> searchFood = mealRepository.findMealByName(newMeal.getName(), newMeal.getDay());
         if (searchFood.isEmpty()) {
-            queryStatus = mealRepository.addMeal(f.getName(), f.getCalories(), f.getDay());  
+            queryStatus = mealRepository.addMeal(newMeal.getName(), newMeal.getCalories(), newMeal.getDay());  
         }
         return queryStatus;
     }
 
     @Transactional
-    public Integer addCalories(Food f){
+    public Integer addCalories(Food newFood){
         Integer queryStatus = Integer.valueOf(-1);
-        queryStatus = mealRepository.addFood(f.getName(),f.getCalories(),LocalDate.now());
+        queryStatus = mealRepository.addFood(newFood.getName(),newFood.getCalories(),LocalDate.now());
         return queryStatus;
     }
 
     public Integer getCalories(String name, LocalDate day){
         // Respond with number of calories or -1 for meal not found
-        this.calories = -1;
         Optional<Meal> qMeal = mealRepository.findMealByName(name, day);
-        qMeal.ifPresent(this::setCalories);
-        return this.calories;
+        if (qMeal.isPresent()){
+            return qMeal.get().getCalories();
+        }
+        return -1;
     }
 
-    public void setCalories(Meal m){
-        this.calories = m.getCalories();
+    public Integer getCaloriesByDay(LocalDate day){
+        Optional<Meal> qMeal = mealRepository.findMealByDay(day);
+        if (qMeal.isPresent()){
+            return qMeal.get().getCalories();
+        }
+        return 0;
     }
 
 }
