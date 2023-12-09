1c79be65c6f26a7765f831d4529d8990d4d9ce6b
Matt Miller <mmiller@crispyecho.com>
2023-12-09 09:43:01 -0500

    Add in flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Build flatpaks for getting older versions of Godot";
+
+  #inputs
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11"; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in {
+        #packages.flatpak-builder = pkgs.flatpak-builder;
+
+        devShell =
+        pkgs.mkShell { 
+          buildInputs = [ pkgs.jdk19 pkgs.maven ]; 
+        };
+   };
+}
