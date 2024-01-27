73220a2cfd3edf7f7caeb65fdcf2e8ce4a7ab1e1
Matt Miller <mmiller@crispyecho.com>
2024-01-27 05:49:16 -0500

    Update comments in flake.nix
    Make comments more meaningful

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   description = "Build diet3 using maven and java with this flake";
 
-  #inputs
+  # inputs - nixos 23.11, flake-utils
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11"; 
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+
+  # outputs - jdk and maven for spring boot
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.x86_64-linux;
