a8f1c15332cb9a36aeb06b1f572e8d3898bd7503
Matt Miller <mmiller@crispyecho.com>
2024-02-09 06:51:18 -0500

    Remove unused imports

--- src/main/java/com/trinsic/diet3/dieter/DieterController.java
+++ src/main/java/com/trinsic/diet3/dieter/DieterController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
-import org.json.*;
 
 @RestController
 @RequestMapping(path = "api/v1/dieter")
