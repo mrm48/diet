51a4871489d2d4312722b45223cd3a3c9a2baed7
Matt Miller <mmiller@crispyecho.com>
2024-05-11 20:22:43 -0400

    Document Diet3Application class

--- src/main/java/com/trinsic/diet3/Diet3Application.java
+++ src/main/java/com/trinsic/diet3/Diet3Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * Entry point for diet3
+ */
 public class Diet3Application {
 
 	public static void main(String[] args) {
