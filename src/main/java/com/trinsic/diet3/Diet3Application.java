24f25ebde39d5aa7687fb9c76df8ae119de55022
Matt Miller <mmiller@crispyecho.com>
2024-05-14 07:12:11 -0400

    Add javadoc for Diet3Application

--- src/main/java/com/trinsic/diet3/Diet3Application.java
+++ src/main/java/com/trinsic/diet3/Diet3Application.java
@@ -3,12 +3,24 @@ package com.trinsic.diet3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 /**
  * Entry point for diet3
+ * @author Matt Miller
  */
+@SpringBootApplication
 public class Diet3Application {
 
+	/**
+	 * No fields required for Diet3Application
+	 */
+	public Diet3Application(){
+
+	}
+
+	/**
+	 * Start the REST API application using Spring Boot
+	 * @param args Command line arguments passed to the application.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Diet3Application.class, args);
 	}
