3d0fa7e80e3a1f3cd3a334d892cf69643d21f177
Matt Miller <mmiller@crispyecho.com>
2024-05-17 11:41:30 -0400

    Simplify the imports as most of the classes in spring-boot annotations are included

--- src/main/java/com/trinsic/diet3/dieter/DieterController.java
+++ src/main/java/com/trinsic/diet3/dieter/DieterController.java
@@ -1,11 +1,6 @@
 package com.trinsic.diet3.dieter;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
