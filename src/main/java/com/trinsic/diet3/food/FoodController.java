f6311093888f096c2899c6f0af4dc7587e70aa11
Matt Miller <mmiller@crispyecho.com>
2024-05-17 10:41:27 -0400

    Importing more from the spring-boot annotations package, simplify import statements to just include the entire package

--- src/main/java/com/trinsic/diet3/food/FoodController.java
+++ src/main/java/com/trinsic/diet3/food/FoodController.java
@@ -1,12 +1,6 @@
 package com.trinsic.diet3.food;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
