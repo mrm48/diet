5398b3444a8f0ae56bd6af257d2720ff0cb5a0e0
Matt Miller <mmiller@crispyecho.com>
2024-09-06 05:58:35 -0400

    Update tests to use a test set of dieters, ensure they are added.

--- mauit_test.go
+++ mauit_test.go
@@ -2,37 +2,68 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"mauit/models"
 	"net/http"
 	"strings"
 	"testing"
 )
 
-var Dieters []models.Dieter
-var Entries []models.Entry
+var Dieters     []models.Dieter
+var Entries     []models.Entry
+var newDieters  []models.Dieter
+var newDieter   models.Dieter
+
 
 func AddDieters() {
 
-    fmt.Printf("Adding Dieters")
-    newdieter := "{\"ID\": 1, \"Name\": \"Matt\", \"Calories\": 1600}"
+    newDieter.Name = "Matt"
+    newDieter.ID = 1
+    newDieter.Calories = 1600
+    newDieters = append(newDieters, newDieter)
+    newDieter.Name = "Jack"
+    newDieter.ID = 2
+    newDieter.Calories = 1600
+    newDieters = append(newDieters, newDieter)
 
-    resp, err := http.Post("http://localhost:9090/dieters/add", "application/json", strings.NewReader(newdieter)) 
+    for _, v := range newDieters {
+        addDieter, err := json.Marshal(v)
+
+        if err != nil {
+            return
+        }
+        resp, err := http.Post("http://localhost:9090/dieters/add", "application/json", strings.NewReader(string(addDieter)))
+
+        if err != nil {
+            return
+        }
+
+        defer resp.Body.Close()
+        dec := json.NewDecoder(resp.Body)
+        var success models.Dieter
+        err = dec.Decode(&success)
+        Dieters = append(Dieters, success)
+    }
+
+}
+
+func QueryDieters() {
+
+    resp, err := http.Get("http://localhost:9090/dieters/all") 
 
     if err != nil {
         return
     }
 
     defer resp.Body.Close()
+
     dec := json.NewDecoder(resp.Body)
-    var success models.Dieter
+    var success []models.Dieter
     err = dec.Decode(&success)
-    Dieters = append(Dieters, success)
+    Dieters = success
 
 }
 
 func AddEntries() {
-    fmt.Printf("Adding Entries")
     Entries = []models.Entry {
         {ID: 1, FoodID:1, MealID:1, Calories:100}, 
         {ID: 2, FoodID:1, MealID:1, Calories:100}, 
@@ -48,8 +79,25 @@ func TestMauit(t *testing.T) {
     want := Dieters != nil 
 
     if !want {
-        t.Errorf("got %v, wanted a list of dieters", got)
-    }
+        t.Errorf("got %v, wanted a new dieter", got)
+    } 
 
+}
+
+func TestQuery(t *testing.T) {
+    QueryDieters()
+
+    got := Dieters
+    want := newDieters 
+
+    if got == nil {
+        t.Errorf("got %v, wanted a list of dieters", got)
+    } else {
+        for k, v := range got { 
+            if v.Name != want[k].Name || v.ID != want[k].ID || v.Calories != want[k].Calories {
+                t.Errorf("got %v, wanted \"%v\"; got %v, wanted %v; got %v, wanted %v", v.Name, want[k].Name, v.ID, want[k].ID, v.Calories, want[k].Calories)
+            }
+        }
+    } 
 }
 
