81ebd6a1879a1133e9eb1e984297f99604a7dfef
Matt Miller <mmiller@crispyecho.com>
2024-10-30 08:12:11 -0400

    Implement dieters/all retrieving from database

--- models/service.go
+++ models/service.go
@@ -1,117 +1,144 @@
 package models
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"log"
+	"net/http"
 )
 
-func GetDieters(context *gin.Context){
+func GetDieters(ctxt *gin.Context) {
 
-    context.IndentedJSON(http.StatusOK, Dieters)
+	db, err := pgx.Connect(context.Background(), "postgresql://postgres@localhost:5432/meal")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query(context.Background(), "Select * FROM dieter")
+	Dieters, err := pgx.CollectRows(rows, pgx.RowToStructByName[Dieter])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, dieter := range Dieters {
+		fmt.Println(dieter.ID)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan()
+	}
+
+	ctxt.IndentedJSON(http.StatusOK, Dieters)
 
 }
 
 // Add specifically a dieter
-func AddDieter(context *gin.Context){
+func AddDieter(context *gin.Context) {
 
-    var n Dieter 
+	var n Dieter
 
-    if err := context.BindJSON(&n); err != nil {
-        return
-    }
+	if err := context.BindJSON(&n); err != nil {
+		return
+	}
 
-    Dieters = append(Dieters, n)
+	Dieters = append(Dieters, n)
 
-    context.IndentedJSON(http.StatusCreated, n)
+	context.IndentedJSON(http.StatusCreated, n)
 
 }
 
 // Get dieter by name
-func GetDieter(context *gin.Context){
-    
-    var d Dieter
+func GetDieter(context *gin.Context) {
 
-    if err := context.BindJSON(&d); err != nil {
-        return
-    }
+	var d Dieter
 
-    for _,v := range Dieters {
-        if v.Name == d.Name {
-            context.IndentedJSON(http.StatusOK, v)
-            return
-        }
-    }
+	if err := context.BindJSON(&d); err != nil {
+		return
+	}
 
-    context.IndentedJSON(http.StatusNotFound, nil)
+	for _, v := range Dieters {
+		if v.Name == d.Name {
+			context.IndentedJSON(http.StatusOK, v)
+			return
+		}
+	}
+
+	context.IndentedJSON(http.StatusNotFound, nil)
 
 }
 
 // Set the calories available for a dieter
-func SetDieterCalories(context *gin.Context){
+func SetDieterCalories(context *gin.Context) {
 
-    var d Dieter 
+	var d Dieter
 
-    if err := context.BindJSON(&d); err != nil {
-        return
-    }
+	if err := context.BindJSON(&d); err != nil {
+		return
+	}
 
-    for _,v := range Dieters {
-        if v.Name == d.Name {
-            SetCalories(v, d.Calories)
-            v.Calories = d.Calories
-            context.IndentedJSON(http.StatusOK, v)
-            return
-        }
-    }
+	for _, v := range Dieters {
+		if v.Name == d.Name {
+			SetCalories(v, d.Calories)
+			v.Calories = d.Calories
+			context.IndentedJSON(http.StatusOK, v)
+			return
+		}
+	}
 
-    context.IndentedJSON(http.StatusNotFound, nil)
+	context.IndentedJSON(http.StatusNotFound, nil)
 
 }
 
 // Set the saved dieter's number of maximum calories
 func SetCalories(d Dieter, c int) {
 
-    for k, v := range Entries {
-        if v.ID == GetID(d) {
-            Dieters[k].Calories = c 
-        }
-    }
+	for k, v := range Entries {
+		if v.ID == GetID(d) {
+			Dieters[k].Calories = c
+		}
+	}
 
 }
 
 // Get maximum number of calories for a dieter
 func GetCalories(d Dieter) int {
 
-    for _, v := range Dieters {
-        if v.ID == d.ID {
-            return v.Calories
-        }
-    }
+	for _, v := range Dieters {
+		if v.ID == d.ID {
+			return v.Calories
+		}
+	}
 
-    return 0
+	return 0
 
 }
 
 // Get the unique ID for the dieter by name
 func GetID(d Dieter) int64 {
 
-    for _, v := range Dieters {
-        if v.Name == d.Name {
-            return v.ID
-        }
-    }
+	for _, v := range Dieters {
+		if v.Name == d.Name {
+			return v.ID
+		}
+	}
 
-    return 0
+	return 0
 
 }
 
 // Set the unique ID for a dieter by name
 func SetID(d Dieter) {
 
-    for k,v := range Dieters { 
-        if v.Name == d.Name {
-            Dieters[k].ID = d.ID
-        }
-    }
+	for k, v := range Dieters {
+		if v.Name == d.Name {
+			Dieters[k].ID = d.ID
+		}
+	}
 
 }
