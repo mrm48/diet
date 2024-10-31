3c8f4994277a0498f47c07aabc8300ca0c83d3c6
Matt Miller <mmiller@crispyecho.com>
2024-10-31 05:57:19 -0400

    Remove initial query of meals, only use the database in services

--- main.go
+++ main.go
@@ -1,41 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"mauit/models"
 	"mauit/router"
-
-	"github.com/jackc/pgx/v5"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
-	db, err := pgx.Connect(context.Background(), "postgresql://postgres@localhost:5432/meal")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err := db.Query(context.Background(), "Select * FROM meal")
-	meals, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Meal])
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, meal := range meals {
-		fmt.Println(meal.ID)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		err = rows.Scan()
-	}
 
 	r := gin.Default()
 
