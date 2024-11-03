ae19644392f5c4173d98f4cabfb2b0a691b97aa7
Matt Miller <mmiller@crispyecho.com>
2024-11-03 11:45:08 -0500

    Fix main formatting

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"mauit/mutils"
 	"mauit/router"
-    "mauit/mutils"
 	"os"
 )
 
@@ -12,20 +12,26 @@ func main() {
 
 	f, err := os.OpenFile("mauit_app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-        log.Fatal(err)
+		log.Fatal(err)
 		return
 	}
-	defer f.Close()
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}(f)
+
 	log.SetOutput(f)
-    mutils.LogMessage("Server Startup", "Initializing")
+	mutils.LogMessage("Server Startup", "Initializing")
 	r := gin.Default()
 
 	router.SetRoutes(r)
 
-    mutils.LogMessage("Server Startup", "Routes set: Starting server")
+	mutils.LogMessage("Server Startup", "Routes set: Starting server")
 
 	// start server
 	r.Run("localhost:9090")
-
 
 }
