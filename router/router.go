e091e52c5487badc7dd5b0e4b3b045933e325381
Matt Miller <mmiller@crispyecho.com>
2024-11-11 07:09:30 -0500

    Update routes to be more concise

--- router/router.go
+++ router/router.go
@@ -6,33 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Add endpoints to the API, noun verb object pathing
+// Add endpoints to the API, noun object pathing
 func SetRoutes(Router *gin.Engine) {
 
 	// all dieters
 	Router.GET("/dieters/all", models.GetDieters)
-	Router.POST("/dieters/add", models.AddDieter)
+	Router.POST("/dieters", models.AddDieter)
 
 	// single dieter
-	Router.GET("/dieter/search/name", models.GetDieter)
-	Router.POST("/dieter/set/calories", models.SetDieterCalories)
-	Router.GET("/dieter/search/calories", models.GetDieterCalories)
-	Router.GET("/dieter/check/calories", models.GetRemainingDieterCalories)
+	Router.GET("/dieter/name", models.GetDieter)
+	Router.POST("/dieter/calories", models.SetDieterCalories)
+	Router.GET("/dieter/calories", models.GetDieterCalories)
+	Router.GET("/dieter/caloriesremaining", models.GetRemainingDieterCalories)
 
 	// meal
-	Router.GET("/meal/search", models.GetMeal)
+	Router.GET("/meal", models.GetMeal)
 	Router.POST("/meal/entry", models.AddEntryToMeal)
-	Router.POST("/meal/add", models.AddMeal)
-	Router.GET("/meal/search/calories", models.GetMealCalories)
+	Router.POST("/meal", models.AddMeal)
+	Router.GET("/meal/calories", models.GetMealCalories)
 
 	// entry
-	Router.GET("/entry/search", models.GetEntry)
+	Router.GET("/entry/", models.GetEntry)
 	Router.POST("/entry/add", models.AddEntry)
 
 	// food
-	Router.POST("/food/add", models.AddFood)
-	Router.POST("/food/set/calories", models.EditFood)
+	Router.POST("/food/", models.AddFood)
+	Router.POST("/food/calories", models.EditFood)
 	Router.GET("/food/all", models.GetAllFood)
-	Router.DELETE("/food/remove", models.DeleteFood)
+	Router.DELETE("/food", models.DeleteFood)
 
 }
