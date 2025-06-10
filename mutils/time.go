e8319358f68d2f48f1cba369ca193713178bb0a3
Matt Miller <mmiller@crispyecho.com>
2025-06-10 07:07:44 -0400

    Add function to get date from body of request

--- mutils/time.go
+++ mutils/time.go
@@ -12,3 +12,9 @@ func GetCurrentDate() string {
 
 	return day
 }
+
+// GetDateFromString will parse a time.Time object from a string that can be used to query the database for a specific day's meals/entries
+func GetDateFromString(date string) (time.Time, error) {
+	parsedDate, err := time.Parse("2006-01-02T15:04:05 -070000", date)
+	return parsedDate, err
+}
