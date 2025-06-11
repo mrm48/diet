fd120f389fd7e2e16d9cdac905c24a56c34a33d9
Matt Miller <mmiller@crispyecho.com>
2025-06-11 05:43:30 -0400

    Update GetDateFromString to return format expected

--- mutils/time.go
+++ mutils/time.go
@@ -14,7 +14,7 @@ func GetCurrentDate() string {
 }
 
 // GetDateFromString will parse a time.Time object from a string that can be used to query the database for a specific day's meals/entries
-func GetDateFromString(date string) (time.Time, error) {
+func GetDateFromString(date string) (string, error) {
 	parsedDate, err := time.Parse("2006-01-02T15:04:05 -070000", date)
-	return parsedDate, err
+	return parsedDate.Format("2006-01-02"), err
 }
