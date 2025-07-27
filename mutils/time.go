8375c00727ba59330d55a935b9d4c59859bcb05b
Matt Miller <mmiller@crispyecho.com>
2025-07-27 06:25:07 -0400

    Remove string literals from function calls in time.go

--- mutils/time.go
+++ mutils/time.go
@@ -4,9 +4,12 @@ import (
 	"time"
 )
 
+var dateTimeStamp string = "2006-01-02T15:04:05 -070000"
+var dateStamp string = "2006-01-02"
+
 // GetCurrentDate Returns the current date as a string for storing meals/entries by day
 func GetCurrentDate() string {
-	day := time.Now().Format("2006-01-02T15:04:05 -070000")
+	day := time.Now().Format(dateTimeStamp)
 
 	day = day[:10]
 
@@ -16,13 +19,13 @@ func GetCurrentDate() string {
 // GetDateFromString will parse a time.Time object from a string and format it correctly so that it can be used to query the database for a specific day's meals/entries
 func GetDateFromString(date string) (string, error) {
 	if date != "" {
-		parsedDate, err := time.Parse("2006-01-02T15:04:05 -070000", date)
+		parsedDate, err := time.Parse(dateTimeStamp, date)
 		if err != nil {
 			return "", err
 		}
-		stringDate := parsedDate.Format("2006-01-02")
+		stringDate := parsedDate.Format(dateStamp)
 		return stringDate, err
 	} else {
-		return time.Now().Format("2006-01-02"), nil
+		return time.Now().Format(dateStamp), nil
 	}
 }
